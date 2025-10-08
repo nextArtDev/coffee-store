11fc80ac443164bcb22fbb724f9d77e701431a93
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2025-10-08 21:00:26 +0330

    debug dynamic100

--- app/(home)/about-us/page.tsx
+++ app/(home)/about-us/page.tsx
@@ -1,15 +1,4 @@
-'use client'
-import dynamic from 'next/dynamic'
-
-// Dynamically import the component with SSR disabled
-const AboutUs = dynamic(() => import('./components/about-us'), {
-  ssr: false,
-  loading: () => (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-primary"></div>
-    </div>
-  ),
-})
+import AboutUs from './components/about-us'
 
 export default function AboutUsPage() {
   return <AboutUs />
--- app/(home)/contact-us/page.tsx
+++ app/(home)/contact-us/page.tsx
@@ -1,15 +1,4 @@
-'use client'
-import dynamic from 'next/dynamic'
-
-// Dynamically import the component with SSR disabled
-const Contact = dynamic(() => import('./components/contact'), {
-  ssr: false,
-  loading: () => (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-primary"></div>
-    </div>
-  ),
-})
+import Contact from './components/contact'
 
 export default function ContactUsPage() {
   return <Contact />
